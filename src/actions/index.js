bbc6362529568f8824ce826ad26b3fdc1c1dd514
renaud dor <renaud.dor@gmail.com>
2020-08-26 12:07:19 +0200

    finish react redux router

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,6 @@ const BASE_URL = 'https://wagon-chat.herokuapp.com';
 
 export const FETCH_MESSAGES = 'FETCH_MESSAGES';
 export const MESSAGE_POSTED = 'MESSAGE_POSTED';
-export const CHANNEL_SELECTED = 'CHANNEL_SELECTED';
 
 export function fetchMessages(channel) {
   const url = `${BASE_URL}/${channel}/messages`;
@@ -29,12 +28,5 @@ export function createMessage(channel, author, content) {
   return {
     type: MESSAGE_POSTED,
     payload: promise // Will be resolved by redux-promise
-  };
-}
-
-export function selectChannel(channel) {
-  return {
-    type: CHANNEL_SELECTED,
-    payload: channel
   };
 }
